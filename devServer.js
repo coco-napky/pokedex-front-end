8d7f0c213b0b316712be9d86f3c6703cfa314274
Brandon Napky <coco.napky@gmail.com>
2016-07-22 17:05:10 -0700

    init

---
+++ devServer.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const config = require('./webpack.config.dev');
+
+const app = express();
+const compiler = webpack(config);
+
+app.set('view engine', 'ejs');
+app.use(express.static('dist'));
+if(process.env.NODE_ENV !== 'production'){
+	app.use(require('webpack-dev-middleware')(compiler, {
+	  noInfo: true,
+	  publicPath: config.output.publicPath
+	}));
+
+	app.use(require('webpack-hot-middleware')(compiler));
+}
+
+app.get('*', (req, res) => {
+  	res.render('index', {production: process.env.NODE_ENV === 'production'});
+});
+
+app.listen(3000, '0.0.0.0', (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:3000');
+  console.log('Running in ' + process.env.NODE_ENV + " mode");
+});
